0ed47015515ddf950128c7625e69a75f80b70c0e
Robin Müller <rm.muller@bluewin.com>
2019-03-01 12:27:31 +0100

    Adding class Carte

--- INF2_Labo3/Labo_3/Partie.h
+++ INF2_Labo3/Labo_3/Partie.h
@@ -2,17 +2,21 @@
 #ifndef PARTIE_H
 #define PARTIE_H
 #include <vector>
+
 #include "Joueur.h"
+#include "Carte.h"
 
 using Joueurs = std::vector<Joueur>;
 using Cartes = std::vector<Carte>;
 
 class Partie{
 public:
-   Partie(Joueurs joueurs, unsigned nb_Famille);
+   Partie(Joueurs joueurs, unsigned nbFamille, );
    Cartes paquetCarte()
 private:
-   
+   const unsigned short NOMBRE_FAMILLES;
+   const unsigned short CARTES_PAR_FAMILLES;
+   const unsigned short NOMBRE_JOUEURS
 };
 
 
