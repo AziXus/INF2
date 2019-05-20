99f70557afca61596bd6477fe56a4bb3a87f7ad6
Naludrag <stephanecarvalho81@gmail.com>
2019-05-20 10:41:11 +0200

    adad

---
+++ INF2_Labo_10/book_index.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   book_index.h
+ * Author: stéphane
+ *
+ * Created on May 20, 2019, 10:39 AM
+ */
+
+#ifndef BOOK_INDEX_H
+#define BOOK_INDEX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOOK_INDEX_H */
+
---
+++ INF2_Labo_10/heading.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   heading.h
+ * Author: stéphane
+ *
+ * Created on May 20, 2019, 10:39 AM
+ */
+
+#ifndef HEADING_H
+#define HEADING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HEADING_H */
+
