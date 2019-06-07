35d9c1074c377277f2a766e1f45ab850d9d9bc56
Robin Müller <rm.muller@bluewin.com>
2019-06-07 12:01:55 +0200

    Labo 11 added

---
+++ INF2_Labo_11/book_index.h
@@ -0,0 +1,66 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 10
+ Fichier     : book_index.h
+ Auteur(s)   : Müller Robin, Stéphane Teixeira Carvalho
+ Date        : 03.06.2019
+
+ But         : Initialiser une structure nommée Element pour créer le principe de liste chainée pour le stockage des différents mots.
+               L'élément pointé Index indique le début de la liste de tout les mot du texte à indexer.
+               On doit pouvoir remplir et détruire la liste.
+
+ Remarque(s) : Les mots à indexer (headings) sont définis ainsi : une suite de caractères alphanumériques sans
+               espace entre eux.
+               Il est composé d'au moins 3 caractères. Les lettres sont converties en minuscules.
+               Les ponctuations accolées aux mots (point ou virgule) sont supprimées.
+
+ Compilateur : MinGW-gcc 6.3.0
+ -----------------------------------------------------------------------------------
+ */ 
+
+#ifndef BOOK_INDEX_H
+#define BOOK_INDEX_H
+#include "heading.h"
+#define MIN_CAR_MOT 3
+//Element allant contenir le mot indexé et l'élément suivant dans la liste
+typedef struct Element Element;
+//Pointeur vers le début de la liste(mots indexés)
+typedef Element* Index;
+
+/**
+ * Renvoie la postion à laquelle ajouter un nouveau mot.
+ * Il renvoie l'élément qui devra être à gauche du nouveau mot.
+ * @param h adresse vers le debut de la liste des mots déjà indexés
+ * @param mot chaîne de caractère constante étant le mot à ajouter
+ * @return la position de l'élément à gauche du nouveau mot.
+ */
+Element* chercherPosition(Index* h, const char* mot);
+
+/**
+ * Permet de créer une liste de mot à indexer vide
+ * @return la liste vide
+ */
+Index* creerIndexVide();
+
+/**
+ * Rempli un index de mot avec un texte passé en paramètre
+ * @param texte chaîne de caractère constante étant le texte à indexer
+ * @return adresse vers le début de la liste avec tous les mots indexés
+ */
+Index* remplirIndex(char* texte);
+
+/**
+ * Permet d'afficher la liste avec les mot indexés
+ * @param h adresse vers la liste des mot indexés
+ */
+void afficherIndex(Index* h);
+
+/**
+ * Permet de détruire(suprrimer) une liste de mot indexer.
+ * Cette fonction détruit l'espace en mémoire pris par la liste chainée.
+ * @param h adresse vers la liste des mot indexés
+ */
+void detruireIndex(Index* h);
+
+#endif /* BOOK_INDEX_H */
+
---
+++ INF2_Labo_11/heading.h
@@ -0,0 +1,62 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 10
+ Fichier     : heading.h
+ Auteur(s)   : Müller Robin, Stéphane Teixeira Carvalho
+ Date        : 03.06.2019
+
+ But         : Initialiser deux structures qui seront sous forme de liste chainée.
+               La structure Location contiendra les numéro du ligne ou le mot à été trouvé.
+               La structure Heading permettra de contenir tout les mot trouvés.
+
+ Remarque(s) : La structure Heading a été créee dans le fichiers heading.h pour permettre à book_index.c d'utiliser 
+               les variables de cette structure.
+
+ Compilateur : MinGW-gcc 6.3.0
+ -----------------------------------------------------------------------------------
+ */
+
+#ifndef HEADING_H
+#define HEADING_H
+#include <stdlib.h>
+
+//Structure permettant de contenir la ligne ou est indexé le mot
+typedef struct Ligne Location;
+
+//Structure contenant le mot ainsi que l'adresse du premier numéro de ligne
+typedef struct
+{
+    const char* mot;
+    Location* premier;
+} Heading;
+
+/**
+ * Insère une ligne dans le heading(mot indexer) passé par paramètre
+ * @param heading adresse de l'objet heading auquel ajouter une ligne
+ * @param noLigne size_t étant la ligne à ajouter
+ */
+void insererLigne(Heading* heading, size_t noLigne);
+
+/**
+ * Permet de créer un objet de type Heading(mot à indexer)
+ * @param mot adresse vers le début d'une chaîne étant le mot à ajouter
+ * @param nombreLigne size_t étant le numéro de la ligne ou est stocké le mot
+ * @return l'adresse de l'objet Heading initialisé
+ */
+Heading* headingCreate(const char* mot, size_t nombreLigne);
+
+/**
+ * Permet de libérer l'espace en mémoire pris par un Heading.
+ * Supprime un heading.
+ * @param h adresse du Heading à supprimer
+ */
+void headingDestroy(Heading* h);
+
+/**
+ * Permet d'afficher les valeurs d'un Heading
+ * @param h adresse du Heading à supprimer
+ */
+void headingPrint(Heading* h);
+
+#endif /* HEADING_H */
+
